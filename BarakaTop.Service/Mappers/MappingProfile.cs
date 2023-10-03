8ae668bf42b0388607c04e2b54a1a3edf6aade53
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:45:50 +0500

    MappingProfile added

---
+++ BarakaTop.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BarakaTop.Domain.Entities;
+using BarakaTop.Service.DTOs.Group;
+using BarakaTop.Service.DTOs.Radar;
+using BarakaTop.Service.DTOs.User;
+
+namespace BarakaTop.Service.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // User
+        CreateMap<User, UserResultDto>().ReverseMap();
+        CreateMap<UserCreationDto, User>().ReverseMap();
+        CreateMap<UserUpdateDto, User>().ReverseMap();
+
+        // Group
+        CreateMap<Group, GroupResultDto>().ReverseMap();
+        CreateMap<GroupCreationDto, Group>().ReverseMap();
+        CreateMap<GroupUpdateDto, Group>().ReverseMap();
+
+        // Radar
+        CreateMap<Radar, RadarResultDto>().ReverseMap();
+        CreateMap<RadarCreationDto, Radar>().ReverseMap();
+    }
+}
