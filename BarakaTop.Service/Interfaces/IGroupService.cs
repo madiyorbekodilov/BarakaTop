46fc7b3c4cd56a83ec95c0a5a35f0f9bca29758d
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:58:07 +0500

    GroupService's Interface added

---
+++ BarakaTop.Service/Interfaces/IGroupService.cs
@@ -0,0 +1,13 @@
+﻿using BarakaTop.Service.DTOs.Group;
+using BarakaTop.Service.DTOs.User;
+
+namespace BarakaTop.Service.Interfaces;
+
+public interface IGroupService
+{
+    Task<GroupResultDto> AddAsync(GroupCreationDto dto);
+    Task<GroupResultDto> ModifyAsync(GroupUpdateDto dto);
+    Task<bool> RemoveAsync(long id);
+    Task<GroupResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<GroupResultDto>> RetrieveAllAsync();
+}
