8b2a327c5d303f594e237c607152b4e3d984ca55
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:53:17 +0500

    RadarService's Interface added

---
+++ BarakaTop.Service/Interfaces/IRadarservice.cs
@@ -0,0 +1,12 @@
+﻿using BarakaTop.Service.DTOs.Radar;
+using BarakaTop.Service.DTOs.User;
+
+namespace BarakaTop.Service.Interfaces;
+
+public interface IRadarservice
+{
+    Task<RadarResultDto> AddAsync(RadarCreationDto dto);
+    Task<bool> RemoveAsync(long id);
+    Task<UserResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<RadarResultDto>> RetrieveAllAsync();
+}
