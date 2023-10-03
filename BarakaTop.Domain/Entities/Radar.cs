d733a2f139f23bc112f0e4c910ae52461ae24eb8
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:01:47 +0500

    RadarDto added

--- BarakaTop.Domain/Entities/Radar.cs
+++ BarakaTop.Domain/Entities/Radar.cs
@@ -7,5 +7,5 @@ public class Radar : Auditable
     public string name { get; set; }
     public string latitude { get; set; }
     public string longitude { get; set; }
-    public int CountOfIgnore { get; set; }
+    public int? CountOfIgnore { get; set; }
 }
---
+++ BarakaTop.Service/DTOs/Radar/RadarResultDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarakaTop.Service.DTOs.Radar
+{
+    internal class RadarResultDto
+    {
+    }
+}
