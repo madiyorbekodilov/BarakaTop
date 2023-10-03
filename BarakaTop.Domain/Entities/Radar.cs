162eaac92ae4b055a390fba63585c9e3e7fe75ed
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-03 23:02:56 +0500

    some properties changed

--- BarakaTop.Domain/Entities/Radar.cs
+++ BarakaTop.Domain/Entities/Radar.cs
@@ -8,6 +8,10 @@ public class Radar : Auditable
     public string latitude { get; set; }
     public string longitude { get; set; }
     public int CountOfIgnore { get; set; }
+
+    public long GroupId { get; set; }
+    public Group Group { get; set; }
+
     public long UserId { get; set; }
     public User User { get; set; }
 }
