4eb829f8a46483e9d790fd48c63f47169aaed443
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-03 22:30:21 +0500

    Radar entity added

---
+++ BarakaTop.Domain/Entities/Radar.cs
@@ -0,0 +1,13 @@
+﻿using BarakaTop.Domain.Commons;
+
+namespace BarakaTop.Domain.Entities;
+
+public class Radar : Auditable
+{
+    public string name { get; set; }
+    public string latitude { get; set; }
+    public string longitude { get; set; }
+    public int CountOfIgnore { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
+}
