e557c9ce5c4fd4043a6040802bba0d07f39ba089
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-03 23:01:44 +0500

    Group entity added

---
+++ BarakaTop.Domain/Entities/Group.cs
@@ -0,0 +1,11 @@
+﻿using BarakaTop.Domain.Commons;
+
+namespace BarakaTop.Domain.Entities;
+
+public class Group : Auditable
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
+}
