e239dbe2cfdc2beb644029b5d67536d95b138a16
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:38:11 +0500

    CustomException added

---
+++ BarakaTop.Service/Exceptions/CustomException.cs
@@ -0,0 +1,10 @@
+﻿namespace BarakaTop.Service.Exceptions;
+
+public class CustomException : Exception
+{
+    public int StatusCode { get; set; }
+    public CustomException(int statusCode,string message) : base(message)
+    {
+        this.StatusCode = statusCode;
+    }
+}
