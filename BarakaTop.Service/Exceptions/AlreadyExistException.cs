6fa563548ccdc4643a2ac0806c0392d5cb7e42a7
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:35:01 +0500

    AlreadyExistException added

---
+++ BarakaTop.Service/Exceptions/AlreadyExistException.cs
@@ -0,0 +1,11 @@
+﻿namespace BarakaTop.Service.Exceptions;
+
+public class AlreadyExistException : Exception
+{
+    public int StatusCode { get; set; }
+    public AlreadyExistException(string message) : base(message)
+    { }
+
+    public AlreadyExistException(string message, Exception innerException) : base(message, innerException)
+    { }
+}
