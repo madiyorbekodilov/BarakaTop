b29abac6f4419722a3c7305f1c26b426aa5d40e5
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-03 22:54:48 +0500

    bugs fixed

--- BarakaTop.Domain/Entities/User.cs
+++ BarakaTop.Domain/Entities/User.cs
@@ -6,7 +6,6 @@ public class User : Auditable
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
     public string Phone { get; set; }
+    public string Password { get; set; }
 }
