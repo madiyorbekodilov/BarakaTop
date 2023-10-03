6e08f5f3db9cac3330057eae05206556a14ae078
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:33:27 +0500

    NotFoundException added

---
+++ BarakaTop.Service/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace BarakaTop.Service.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public int StatusCode { get; set; } = 404;
+    public NotFoundException(string message) : base(message)
+    { }
+
+    public NotFoundException(string message, Exception innerException) : base(message, innerException)
+    { }
+}
