0e19d75431240b71a2a571d8c23fe1fbe3b4440e
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-03 22:51:34 +0500

    Repasitory added

--- BarakatTop.Data/IRepasitories/IRepasitory.cs
+++ BarakatTop.Data/IRepasitories/IRepasitory.cs
@@ -5,9 +5,8 @@ namespace BarakatTop.Data.IRepasitories;
 
 public interface IRepasitory<T> where T : Auditable
 {
-    ValueTask CreateAsync(T entity);
+    ValueTask<T> CreateAsync(T entity);
     void Update(T entity);
-    void Delete(T entity);
     void Destroy(T entity);
     ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null);
     IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, bool isNoTracked = true, string[] includes = null);
---
+++ BarakatTop.Data/Repasitories/Repasitory.cs
@@ -0,0 +1,65 @@
+﻿using BarakaTop.Domain.Commons;
+using BarakatTop.Data.Contexts;
+using BarakatTop.Data.IRepasitories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BarakatTop.Data.Repasitories;
+
+public class Repasitory<T> : IRepasitory<T> where T : Auditable
+{
+    private readonly AppDbContext appDbContext;
+    private readonly DbSet<T> dbSet;
+    public Repasitory(AppDbContext appDbContext)
+    {
+        this.appDbContext = appDbContext;
+        dbSet = this.appDbContext.Set<T>();
+    }
+
+    public async ValueTask<T> CreateAsync(T entity)
+    {
+        await this.dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public void Destroy(T entity)
+    {
+        this.dbSet.Remove(entity);  
+    }
+
+    public async ValueTask SaveAsync()
+    {
+        await appDbContext.SaveChangesAsync();
+    }
+
+    public IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, bool isNoTracked = true, string[] includes = null)
+    {
+        IQueryable<T> query = expression is null ? dbSet.AsQueryable() : dbSet.Where(expression).AsQueryable();
+
+        query = isNoTracked ? query.AsNoTracking() : query;
+
+        if (includes is not null)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+        return query;
+    }
+
+    public async ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null)
+    {
+        IQueryable<T> query = dbSet.Where(expression).AsQueryable();
+
+        if (includes is not null)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+        var entity = await query.FirstOrDefaultAsync(expression);
+        return entity;
+    }
+
+    public void Update(T entity)
+    {
+        entity.UpdateAt = DateTime.UtcNow;
+        appDbContext.Entry(entity).State = EntityState.Modified;
+    }
+}
