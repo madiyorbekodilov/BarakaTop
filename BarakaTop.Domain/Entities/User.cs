af37084f6096ecf3a3d81906459b87fe14bd999f
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-03 17:45:44 +0500

    User Entity added

---
+++ BarakaTop.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using BarakaTop.Domain.Commons;
+
+namespace BarakaTop.Domain.Entities;
+
+public class User : Auditable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+}
