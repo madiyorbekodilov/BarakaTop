b64bc22d9494da1a5ad46a4d855c76f270a6670d
Ferganskiy <odilovmadiyor029@gmail.com>
2023-10-04 00:29:44 +0500

    bugs fixed

--- BarakaTop.Service/DTOs/User/UserUpdateDto.cs
+++ BarakaTop.Service/DTOs/User/UserUpdateDto.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BarakaTop.Service.DTOs.User;
 
-namespace BarakaTop.Service.DTOs.User
+public class UserUpdateDto
 {
-    internal class UserUpdateDto
-    {
-    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
 }
